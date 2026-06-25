2cc5729f9b65f8e27305668fc4f1317c850d3bf4
FortuneBela <zachknifex@gmail.com>
2026-06-24 21:24:56 -0400

    First save and load system

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import json
+import os
 
 class Wheel:
     def __init__(self):
@@ -9,6 +11,14 @@ class Wheel:
 
     def remove_section(self, index):
         return self.sections.pop(index)
+    
+    def save(self, filename):
+        with open(filename, "w") as file:
+            json.dump(self.sections, file)
+
+    def load(self, filename):
+        with open(filename, "r") as file:
+            self.sections = json.load(file)
     
     def is_empty(self):
         return len(self.sections) == 0
@@ -36,7 +46,9 @@ while True:
     print("2. Remove Section")
     print("3. View Current Roulette")
     print("4. Spin Roulette")
-    print("5. Exit")
+    print("5. Save Current Roulette")
+    print("6. Load Saved Roulette")
+    print("7. Exit")
 
     choice = input("Make your selection: ").strip()
 
@@ -89,6 +101,21 @@ while True:
             print("Winner:", result)    
 
     elif choice == "5":
+        name = input("Please enter the name you wish to save the file as: ").strip()
+        filename = f"{name}.json"
+        wheel.save(filename)
+
+    elif choice == "6":
+        name = input("Please enter the name of the file you wish to load: ").strip()
+        filename = f"{name}.json"
+        if os.path.exists(filename):
+            print("File Found!")
+            wheel.load()
+        else: 
+            print("File Not Found.")
+        
+
+    elif choice == "7":
         print("Thank you for using Fortunate Roulette!")
         break
 
